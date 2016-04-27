96bb672657770fc9f20a9908b6301bd5db85fa6a
XenoAron <jwt1029@naver.com>
2016-04-27 19:16:28 +0900

    test reqbody text

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require("http");
 var url = require("url");
+var querystring = require("querystring");
 
 function go(route, handle) {
 	function onRequest(request, response) {
-		console.log("Received data : " + request.body);
+		var text = querystring.parse(request.body);
+		console.log("Received data : " + text + " | " + text.text);
 		var pathname = url.parse(request.url).pathname;
 		
 		route(handle, pathname, response);
