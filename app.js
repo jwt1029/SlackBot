11b309423a0584b0dcb06c25587fd89e976889d3
XenoAron <jwt1029@naver.com>
2016-04-29 10:40:06 +0900

    port num

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function go(route, handle) {
 		}
 	}
 
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(process.env.PORT);
 	console.log("Server has started!");
 }
 
